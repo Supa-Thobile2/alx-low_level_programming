633c9b840c42cd83b3a4e9133755de5e9f1b92af
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-10-13 15:14:22 -0700

    updated task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main-entry point for the program.
- *   *
- *    * Return:0 if no errors, return non zero if errors
- *     */
+ * main-Printing
+ *
+ * Return: Always 0 (successful)
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
