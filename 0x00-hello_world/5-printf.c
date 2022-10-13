80b8a9d90cf62c7e275aa9caaabeb9ca018eead9
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-10-13 03:18:15 -0700

    updated project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-Printing
+ *
+ * Return: Always 0 (is successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is art, \n");
+	return (0);
+}
