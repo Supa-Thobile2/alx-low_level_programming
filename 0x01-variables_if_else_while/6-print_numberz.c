39cfc42f41c0cdde68c659e4449fc582f8a32958
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-10-17 19:10:10 -0700

    updated project

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,16 @@
 /**
  * main - Prints numbers between 0 to 9.
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+
 	for (i = 48; i < 58; i++)
+	{
 		putchar(i);
+	}
 	putchar('\n');
 	return (0);
 }
