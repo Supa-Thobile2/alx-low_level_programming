f7aa52ca22ed9a8ef08b5408ea0a227893643bae
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-10-22 12:54:43 -0700

    updated project

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,8 +10,6 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		
-
 		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
@@ -39,10 +37,8 @@ void times_table(void)
 				if (b < 9)
 				{
 					_putchar(',');
-                                        _putchar(' ');
-
+					_putchar(' ');
 				}
-
 			}	
 		}
 		_putchar('\n');
