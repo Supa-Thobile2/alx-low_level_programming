5c0d8414eb5e81b98054f123ab90ea3405cb9132
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-10-22 13:44:54 -0700

    updated project

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
  */
 void print_numbers(void)
 {
-	int a = 0;
+	char c;
 
-	while (a < 10)
-		_putchar(a++ + '0');
-
-	_putchar("\n");
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
 
