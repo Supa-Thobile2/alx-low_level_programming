63804b0367db159e11f60716fc384fbebdd8ecc2
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-10-28 10:15:48 -0700

    updated project

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * islower - checks if character is lower case
+ * _islower - checks if character is lower case
  * @c: the character
  * Return: 1 if letter is lower case and 0 if not
  */
