1793aecb31944c2d543cad33355a158b844ef816
Supa-Thobile2 <thobilenontenja@gmail.com>
2022-11-02 17:39:17 -0700

    updated project

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,9 +11,7 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return (0);
+	len = 1 + _strlen_recursion(s + 1);
 }
