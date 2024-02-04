abeb20f131f85d50612532ea6fb78747a3192383
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-04 06:34:27 -0800

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,13 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-	int num,j,result;
+	int num, j, result;
 		int coins[] = {25, 10, 5, 2, 1};
 
-	if(argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
-				return(1);
+				return (1);
 	}
 
 	num = atoi(argv[1]);
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	if (num > 0)
 	{
 		printf("0\n");
-		return(0);
+		return (0);
 	}
 	for (j = 0; j < 5; j++)
 	{
