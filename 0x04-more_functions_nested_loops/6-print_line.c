f9793a2038a2b3ce7e67147794d4d059732452e9
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-05 03:02:04 -0800

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,14 +2,14 @@
 
 /**
  * print_line - prints a straightline using
- *
+ *@n: number of times lines will be printed
  * Return: Always 0.
  */
 void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i - 0; i < n; i++)
 	{
 		_putchar('_');
 	}
