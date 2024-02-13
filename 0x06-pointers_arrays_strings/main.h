dfe4d04a18c030a2093e59e11f4833364c1de29a
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-13 08:17:42 -0800

    magic

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -8,10 +5,9 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-char *_strncat(char *dest, char *src, int n);
+char *leet(char *);
+char *rot13(char *);
 void print_number(int n);
-int _putchar(char c);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-#endif
-
+void print_buffer(char *b, int size);
+int _putchar(char c);
