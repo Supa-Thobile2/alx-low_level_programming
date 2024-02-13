d95a6aaff9e78bd05e6b1e831d8be3b9617abb39
Supa-Thobile2 <thobilenontenja198@gmail.com>
2024-02-13 02:58:28 -0800

    added 5-flip_bits.c

--- 0x14-bit_manipulation/5-main.c
+++ 0x14-bit_manipulation/5-main.c
@@ -12,8 +12,10 @@ int main(void)
 
 	n = flip_bits(1024, 1);
 	printf("%u\n", n);
-	n = flip_bits(402, 98);printf("%u\n", n);
-	n = flip_bits(1024, 3);printf("%u\n", n);
+	n = flip_bits(402, 98);
+	printf("%u\n", n);
+	n = flip_bits(1024, 3);
+	printf("%u\n", n);
 	n = flip_bits(1024, 1025);
 	printf("%u\n", n);
 	return (0);
